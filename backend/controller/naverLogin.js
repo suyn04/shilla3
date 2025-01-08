bfa294277c160163aa9a1c4b778afc88374b5ef2
woochrystal <woochryfront@gmail.com>
2025-01-08 16:07:11 +0900

    네이버 로그인 값 가져오기

--- backend/controller/naverLogin.js
+++ backend/controller/naverLogin.js
@@ -1,64 +1,91 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../db");
-const naverLogin = require("./naverLoginInfo");
-const dotenv = require('dotenv');
-dotenv.config();
+const axios = require('axios');
+const session = require("express-session");
+const {CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, STATE} = require('./naverLoginInfo');
+let naverLoginUrl = ``
+
 
 
 // 인덱스에서 넘기는 자료를 받아서 처리
-module.exports = () => {
-
-    router.get("/", async (req, res) => {//네이버 로그인/bk/login/naver
-        const state = Math.random().toString(36).substring(7);
-        req.session.state = state; // 세션에 state 저장
-        console.log("login 목록 접근", req.session);
-
-        const client_id = naverLogin.clientId;
-        const redirectURI = encodeURIComponent(naverLogin.redirectURI);
-        const naverLoginUrl 
-        = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${client_id}&state=${state}&redirect_uri=${redirectURI}`;
+module.exports = () => {//이미 로그인 돼있다면 이 화면은 생략되고 callback으로 이동
+    
+    router.get("/", async (req, res) => {//네이버 로그인/bk/naverLogin
+        req.session.state = STATE; // 세션에 state 저장
+        // console.log("login 목록 접근", req.session);
+        // console.log(req.session.state)
+        naverLoginUrl 
+        = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
         res.redirect(naverLoginUrl);//네이버 로그인 페이지로 이동
-        // console.log(naverLoginUrl);
+        // return res.redirect(naverLoginUrl);//네이버 로그인 페이지로 이동
+        // res.end("<a href='"+ naverLoginUrl + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+        
         
     });
 
     router.get("/callback", async (req, res) => {
         const { code, state } = req.query;
-        if(state !== req.session.state){
-            return res.status(400).send('status mismatch')
+        console.log(req.query, code, state, req.session.state)
+        if(state != req.session.state){
+            //보안 강화를 위함
+            return res.status(400).send('state 값 일치하지 않음')
         }
-        try{
-            const client_id = naverLogin.clientId;
-            const client_secret = naverLogin.clientSecret;
-            const redirectURI = encodeURIComponent(naverLogin.redirectURI);
+        // 액세스 토큰 요청
+        // const tokenUrl = 'https://nid.naver.com/oauth2.0/token';
+        naverLoginUrl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=`
+                        + CLIENT_ID+`&client_secret=`+CLIENT_SECRET+`&redirect_uri=`+REDIRECT_URI+`&code=`+code+`&state=`+state;
+        
+        const response = await fetch(naverLoginUrl,{
+            headers: {
+                'X-Naver-Client-Id':CLIENT_ID, 
+                'X-Naver-Client-Secret': CLIENT_SECRET
+            }
+            
+        })
+        const tokenRequest = await response.json();
+        // return res.send(tokenRequest);
 
-            //API로 토큰 요청 방식 
-            const response = await axios.post('https://nid.naver.com/oauth2.0/token', null, {
-                params: {
-                  grant_type: 'authorization_code',
-                  client_id: client_id,
-                  client_secret: client_secret,
-                  code: code,
-                  state: state,
-                  redirect_uri: redirectURI,
-                }
+        if ("access_token" in tokenRequest) {
+            const { access_token } = tokenRequest;
+            const apiUrl = "https://openapi.naver.com/v1/nid/me";
+        
+            const data = await fetch(apiUrl, {
+              headers: {
+                Authorization: `Bearer ${access_token}`,
+              },
             });
-            const accessToken = response.data.access_token;//액세스 토큰
-
-            // 사용자 정보 요청
-            const userInfo = await axios.get('https://openapi.naver.com/v1/nid/me', {
-                headers: {
-                Authorization: `Bearer ${accessToken}`,
-                }
-            });
-            res.json(userInfo.data);//사용자 정보 반환
-
+        
+            const userData = await data.json();
+        
+            //사용자 정보 콘솔로 받아오기 -> DB에 저장하기 
+            console.log("userData:", userData);
+          }
+        
+          return res.send("DB에 저장하고 랜드페이지로 redirect ");
+        });
+    /*
+        //네이버 예제 코드인데 request 모듈사용에서 에러 발생
+        var options = {
+            url : naverLoginUrl,
+            headers: {
+                'X-Naver-Client-Id':CLIENT_ID, 
+                'X-Naver-Client-Secret': CLIENT_SECRET
+            }
         }
-        catch(err){
-            console.error(err)
-            res.status(500).send('네이버 로그인 사용자 정보 받아오기 오류')
-        }
-    })
+
+        request.get(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              res.writeHead(
+                200, {
+                    'Content-Type': 'text/json;charset=utf-8'
+                });
+              res.end(body);
+            } else {
+              res.status(response.statusCode).end();
+              console.log('error = ' + response.statusCode);
+            }
+        });*/
+      
     return router;
 }
--- backend/controller/naverLoginInfo.js
+++ backend/controller/naverLoginInfo.js
@@ -1,6 +1,12 @@
+const CLIENT_ID = '0qPsakx_WEZQuBwtEcAv';
+const CLIENT_SECRET = 'f8TYOCPl3e';
+// REDIRECT_URI은 네이버에 등록된 콜백 URL을 입력해야한다
+const REDIRECT_URI = 'http://localhost:5002/bk/naverLogin/callback';
+const STATE = Math.random().toString(36).substring(7);
+
 module.exports = {//네이버 로그인 정보 - .env
-    clientId: process.env.REACT_APP_CLIENT_ID,
-    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-    state: process.env.REACT_APP_STATE,
-    redirectURI: process.env.REACT_APP_REDIRECT_URI
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI,
+    STATE
 };
